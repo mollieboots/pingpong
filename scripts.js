7e696c250ba5911ec5dd5a9fb3efd5ff7a902d18
Mollie <mollie@civil.cm>
2016-01-22 10:38:55 -0800

    reworked specs

--- scripts.js
+++ scripts.js
@@ -1,30 +1,60 @@
-var pingpong = function(inputNumber) {
-	if ((inputNumber % 5 === 0) && (inputNumber % 3 === 0)) {
-		return "pingpong";
-	} else if (inputNumber % 3 === 0) {
+var createArray = function(inputNumber) {
+	var integerArray = [];
+	for (var inputNumber = inputNumber; inputNumber > 0 ; inputNumber -= 1) {
+		integerArray.push(inputNumber);
+	}
+	return integerArray;
+};
+
+var divisibleByThree = function(inputNumber) {
+	if (inputNumber % 3 === 0) {
 		return "ping";
-	} else if (inputNumber % 5 === 0) {
-		return "pong";
-	} else {
-		return inputNumber;
 	}
 };
 
+var divisibleByFive = function(inputNumber) {
+	if (inputNumber % 5 === 0) {
+		return "pong";
+	}
+};
+
+var divisibleByThreeAndFive = function(inputNumber) {
+	if ((inputNumber % 5 === 0) && (inputNumber % 3 === 0)) {
+		return "pingpong";
+	}
+};
+
+var pingpong = function(inputNumber) {
+	var integerArray = createArray(inputNumber);
+	
+	integerArray.forEach(function(element, index) {
+		if (divisibleByThreeAndFive(element) === 'pingpong') {
+			integerArray[index] = 'pingpong';
+		} else if (divisibleByThree(element) === 'ping') {
+			integerArray[index] = 'ping';
+		} else if (divisibleByFive(element) === 'pong') {
+			integerArray[index] = 'pong';
+		}
+	});
+	return integerArray;
+}
+
 $(document).ready(function() {
-	$("button#submit").click(function(event) {
+	$("button#submit").click(function(event){
+		event.preventDefault();
+		
 		var inputNumber = parseInt($("input#putnumber").val());
 		var result = pingpong(inputNumber);
 		
 		if (result === "ping") {
-			alert("ping");
+			$("ul#output").prepend("<li>Ping</li>");
 		} else if (result === "pong") {
-			alert("pong");
+			$("ul#output").prepend("<li>Pong</li>");
 		} else if (result === "pingpong") {
-			alert("pingpong");
-		} 
-		$( '#putnumber' ).each(function(){
-			this.reset();
-		});
-		event.preventDefault();
+			$("ul#output").prepend("<li>Pingpong</li>");
+		}
+		
+		$("input#putnumber").reset();
+		
 	});
 });
--- specs.js
+++ specs.js
@@ -1,13 +1,29 @@
-describe('pingpong', function() {
-	it("will check to see if thee inputNumber is divisible by 3, and if so, return 'ping'", function() {
-		expect(pingpong(9)).to.equal("ping");
+describe('createArray', function() {
+	it("will create an array and fill it with all integers from 1 to the inputNumber", function() {
+		expect(createArray(12)).to.eql([12,11,10,9,8,7,6,5,4,3,2,1]);
 	});
+});
+
+describe('divisibleByThree', function() {
+	it("will check to see if the inputNumber is divisible by 3, and if so, return 'ping'", function() {
+		expect(divisibleByThree(9)).to.equal("ping");
+	});
+});
 	
+describe('divisibleByFive', function() {
 	it("will check to see if the inputNumber is divisible by 5, and if so, return 'pong'", function() {
-		expect(pingpong(20)).to.equal("pong");
+		expect(divisibleByFive(20)).to.equal("pong");
 	});
-	
+});
+
+describe('divisibleByThreeAndFive', function() {
 	it("will check to see if a number is divisible by 3 and 5, and if so, will return 'pingpong'", function() {
-		expect(pingpong(15)).to.equal("pingpong");
+		expect(divisibleByThreeAndFive(15)).to.equal("pingpong");
+	});
+});
+
+describe('pingpong', function() {
+	it("will search through array and convert all numbers divisible by 3, 5, and 3&5 to their respective strings", function() {
+		expect(pingpong(15)).to.eql(["pingpong",14,13,"ping",11,"pong","ping",8,7,"ping","pong",4,"ping",2,1]);
 	});
 });
